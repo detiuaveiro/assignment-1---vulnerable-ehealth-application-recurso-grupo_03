7e9465062e9baa72cfa6ed3108e7e4c125300c4e
v1centebarros <vmabarros@gmail.com>
2022-10-17 17:33:08 +0100

    First setup

---
+++ app/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, url_for
+from os import path, environ
+import pymysql
+from dotenv import load_dotenv
+
+TEMPLATE_DIR = path.abspath('./templates')
+app = Flask(__name__,template_folder=TEMPLATE_DIR)
+load_dotenv(".env")
+
+connection = pymysql.connect(host=environ.get("MYSQL_HOST"), port=int(environ.get("MYSQL_PORT")), user='admin', password='admin', database='ecorp', cursorclass=pymysql.cursors.DictCursor)
+
+@app.route('/')
+def index():
+    # with connection.cursor() as cursor:
+    #     cursor.execute("SELECT * FROM teste")
+    #     result = cursor.fetchall()
+    # return render_template('index.html', users=result)´
+    return render_template('index.html')
+
+@app.route('/checkdb')
+def checkDB():
+    with connection.cursor() as cursor:
+        cursor.execute("SHOW STATUS")
+        result = cursor.fetchall()
+        return result
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000, debug=True)
