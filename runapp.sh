839d0ccb4e8bb07420e4051fc0caf5e3f2da61a3
Gui <guicostaantunes@gmail.com>
2022-11-16 08:31:57 +0000

    delete other runs

--- runapp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export FLASK_APP=app/__init__.py
-export FLASK_ENV=development
-export FLASK_DEBUG=1
-export FLASK_RUN_PORT=5050
-flask run
--- runappsec.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export FLASK_APP=app_sec/__init__.py
-export FLASK_ENV=development
-export FLASK_RUN_PORT=5051
-export FLASK_DEBUG=1
-
-flask run
