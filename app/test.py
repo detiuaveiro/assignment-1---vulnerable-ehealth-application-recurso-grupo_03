c40c7af3b55b6a523e35bd3c0fdfe94a4c56a5ed
Gui <guicostaantunes@gmail.com>
2022-11-17 01:22:56 +0000

    n mexo mais nisto pt3

--- app/test.py
+++ app/test.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 from flask import render_template,make_response
 import pdfkit
 
-from app_sec.models import User, Report
+from app.models import User, Report
 
 tst = Blueprint("tst", __name__)
 
@@ -19,11 +19,9 @@ def test(user_id=None):
     response.headers["Content-Type"] = "application/pdf"
     response.headers["Content-Disposition"] = "inline; filename=output.pdf"
     return response
-
 @tst.route("/tests", methods=["GET"])
 def tests():
     return render_template("tests.html")
-
 @tst.route("/test/", methods=["POST"])
 def generate_link():
     code = request.form.get("code")
@@ -32,42 +30,3 @@ def generate_link():
         return "No report found"
     else:
         return "Your link has been generated. Click <a href='/test/"+str(report.patientId)+"/'>here</a> to view your report."
-{% extends "base.html" %}
-
-{% block container %}
-<body>
-{%  include "navbar.html" %}
-<div class="container">
-    <div class="row">
-        <h1>Tests</h1>
-        <input id="code" type="search" name="code" class="form-control"/>
-        <button id="search" type="submit" class="btn btn-primary mt-2">Search</button>
-        <div id="message-link" class="mx-auto pt-2"></div>
-    </div>
-</div>
-</body>
-{% endblock %}
-
-{% block scripts %}
-<script>
-    $(document).ready(function() {
-        $("#search").click(function() {
-            $.ajax({
-                url: "/test",
-                type: "POST",
-                data: {
-                    code: $("#code").val()
-                },
-                headers: {
-                    "X-CSRFToken": "{{ csrf_token() }}"
-                },
-                success: function(data) {
-                    console.log(data);
-                    $("#message-link").html(data);
-
-                }
-            });
-        });
-    });
-</script>
-{% endblock %}
