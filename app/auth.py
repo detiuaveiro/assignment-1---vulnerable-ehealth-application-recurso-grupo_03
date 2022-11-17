a4c31a7319552fbb0d51e89ffae528414d25cbaf
v1centebarros <vmabarros@gmail.com>
2022-11-17 01:48:51 +0000

    Admin stupified

--- app/auth.py
+++ app/auth.py
@@ -17,6 +17,13 @@ def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
 
+    if email == 'admin' and password == 'admin':
+        user = User.query.filter_by(email="admin@admin.com").first()
+        if user:
+            login_user(user, remember=True)
+            return redirect(url_for('profile.profile'))
+        else:
+            return redirect(url_for('auth.login'))
 
     result = db.session.execute("SELECT * FROM user WHERE email = '"+email+"' AND password = '"+password+"';").fetchall()
 
